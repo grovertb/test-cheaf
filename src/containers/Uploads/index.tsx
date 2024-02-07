6e701d05133b147b9503424ca5929505d0f2f91e
GroverTB <grover.jtb@gmail.com>
2024-02-07 13:16:10 -0500

    [ UPDATE ] toolbar in uploads

--- src/containers/Uploads/index.tsx
+++ src/containers/Uploads/index.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { ImageList, ImageListItem, Paper } from '@mui/material'
+import { AppBar, IconButton, ImageList, ImageListItem, Paper, Toolbar, Typography } from '@mui/material'
 import { collection, onSnapshot } from 'firebase/firestore'
+import { ArrowBackRounded as ArrowBackRoundedIcon } from '@mui/icons-material'
 
 import { firestore, listAllImages } from '../../utils/firebase'
 import UploadButton from '../../components/Uploads/UploadButton'
 
 import Root, { classes } from './styles'
+import { useNavigate } from 'react-router-dom'
 
 const Uploads: React.FC = () => {
+  const navigate = useNavigate()
   const [ images, setImages ] = useState<string[]>([])
 
   useEffect(() => {
@@ -26,27 +29,49 @@ const Uploads: React.FC = () => {
     return () => unsubscribe()
   }, [])
 
+  const _handleClickGoHome = () => {
+    navigate('/home')
+  }
+
   return (
-    <Root>
-      <UploadButton />
-      <ImageList
-        cols={6}
-        gap={16}
-        rowHeight={164}
-        sx={{ pb: 1 }}>
-        {
-          images.map((imageUrl, index) => (
-            <ImageListItem component={Paper} key={index}>
-              <img
-                alt={`upload-${index}`}
-                className={classes.imageCard}
-                loading='lazy'
-                src={imageUrl} />
-            </ImageListItem>
-          ))
-        }
-      </ImageList>
-    </Root>
+    <>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            aria-label='menu'
+            color='inherit'
+            edge='start'
+            onClick={_handleClickGoHome}
+            size='large'
+            sx={{ mr: 2 }}>
+            <ArrowBackRoundedIcon />
+          </IconButton>
+          <Typography component='div' sx={{ flexGrow: 1 }} variant='h6'>
+            Uploads
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Root>
+        <UploadButton />
+        <ImageList
+          cols={6}
+          gap={16}
+          rowHeight={164}
+          sx={{ pb: 1 }}>
+          {
+            images.map((imageUrl, index) => (
+              <ImageListItem component={Paper} key={index}>
+                <img
+                  alt={`upload-${index}`}
+                  className={classes.imageCard}
+                  loading='lazy'
+                  src={imageUrl} />
+              </ImageListItem>
+            ))
+          }
+        </ImageList>
+      </Root>
+    </>
   )
 }
 
